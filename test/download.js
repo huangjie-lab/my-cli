105cd588c188d686d13fa6fd0b7a780ce2b83aef
huangjie <huangjie6@codemao.cn>
2024-12-06 14:59:16 +0800

    ✨ feat: 新增下载代码到本地

--- test/download.js
+++ test/download.js
@@ -1,5 +1,31 @@
 const download = require('download-git-repo')
+const chalk = require('chalk');
+const ora = require('ora');
+const successCb = (info) => {
+    console.log(
+        chalk.green(info),
+    );
+}
+// 给代码downloading提示
+const spinner = ora('downloading code...').start(); 
+const cb = (res) => {
+    console.log(res);
+    if(!res){
+        spinner.color = 'yellow';
+        spinner.text = 'downloading success';
+        setTimeout(() => {
+            spinner.stop();
+            successCb('downloading success')
+        }, 1500);
+    }else{
+        spinner.color = 'red';
+        spinner.text = 'downloading failed';
+        setTimeout(() => {
+            spinner.stop();
+            successCb('downloading failed reason' + res)
+        }, 1500);
+    }
+    
+}
+download('direct:https://github.com/huangjie-lab/testgit.git#main','./download',cb)
 
-// download('direct:','git@github.com:huangjie-lab/my-cli.git','./xxx',{clone:true},(res) => {
-//     console.log(res);
-// })
